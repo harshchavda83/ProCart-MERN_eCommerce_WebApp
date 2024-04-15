1b16157257be9a5b5f74e9baac89f28d8b004402
Harsh Chavda <chavdaharsh120@gmail.com>
2024-04-15 13:34:48 +0530

    Intial

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Paginate from "../components/Paginate";
+import Meta from "../components/Meta";
+import ProductCarousek from "../components/ProductCarousek";
+
+const HomeScreen = () => {
+  const { pageNumber, keyword } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({
+    keyword,
+    pageNumber,
+  });
+
+  return (
+    <>
+      {!keyword ? (
+        <ProductCarousek />
+      ) : (
+        <Link to="/" className="btn btn-light mb-4">
+          Go Back
+        </Link>
+      )}
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          {" "}
+          <Meta title="Shop" />
+          <h1>Latest Products</h1>
+          <Row>
+            {data.products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={data.pages}
+            page={data.page}
+            keyword={keyword ? keyword : ""}
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
